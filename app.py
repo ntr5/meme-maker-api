ba6b0451b5c979462611c1a900a5498e78a72527
pingt9*t <pressntr@gmail.com>
2020-01-20 10:02:01 -0700

    added post route

---
+++ app.py
@@ -0,0 +1,73 @@
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+from flask_marshmallow import Marshmallow
+from flask_heroku import Heroku
+import os
+
+app = Flask(__name__)
+Heroku = Heroku(app)
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(basedir, "app.sqlite")
+
+CORS(app)
+
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
+
+class Meme(db.Model):
+    _tablename__ = "memes"
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(50))
+    image = db.Column(db.String(500))
+    favorite = db.Column(db.Boolean)
+
+    def __init__(self, text, image, favorite):
+        self.text = text
+        self.image = image
+        self.favorite = favorite
+
+
+class MemeSchema(ma.Schema):
+    class Meta:
+        fields = ("id", "text", "image", "favorite")
+
+
+meme_schema = MemeSchema()
+memes_schema = MemeSchema(many=True)
+
+
+@app.route("/")
+def greeting():
+    return "<h1>Meme Maker API</h1>"
+
+
+#POST
+@app.route("/add-meme", methods=["POST"])
+def add_meme(): 
+    text = request.json["text"]
+    image = request.json["image"]
+    favorite = request.json["favorite"]
+
+    new_meme = Meme(text, image, favorite)
+
+    db.session.add(new_meme)
+    db.session.commit()
+
+    return jsonify("MEME POSTED")
+
+
+
+
+#GET
+#PUT
+#DELETE
+
+
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
+
+
